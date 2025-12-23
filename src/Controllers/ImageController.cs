71111f36f6659ceafe33e552dc87c70560abcdbe
Chibuzor Victor Umezuruike <41339694+umezvictor@users.noreply.github.com>
2025-12-23 13:59:02 +0200

    first commit

---
+++ src/Controllers/ImageController.cs
@@ -0,0 +1,56 @@
+using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageResizerAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageController : ControllerBase
+    {
+
+        private const string S3BucketName = "uploads-app-bucket";
+        private readonly IAmazonS3 _s3Client;
+        public ImageController(IAmazonS3 s3Client)
+        {
+            _s3Client = s3Client;
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadImage(IFormFile file)
+        {
+
+            if (file.Length == 0)
+                return BadRequest("No file uploaded.");
+
+            using var stream = file.OpenReadStream();
+            var key = Guid.NewGuid();
+            var putRequest = new PutObjectRequest
+            {
+                BucketName = S3BucketName,
+                Key = $"images/{key}",
+                InputStream = stream,
+                ContentType = file.ContentType
+            };
+
+            await _s3Client.PutObjectAsync(putRequest);
+            return Ok(new { Message = "File uploaded to S3!", S3Key = key });
+
+        }
+
+        [HttpGet("download")]
+        public async Task<IActionResult> Download(string key)
+        {
+            var getRequest = new GetObjectRequest
+            {
+                BucketName = S3BucketName,
+                Key = $"images/{key}",
+            };
+            var response = await _s3Client.GetObjectAsync(getRequest);
+            return File(response.ResponseStream, response.Headers.ContentType);
+        }
+    }
+}
+
+
+
---
+++ src/Program.cs
@@ -0,0 +1,28 @@
+using Amazon.S3;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
+
+builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
+builder.Services.AddAWSService<IAmazonS3>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+app.MapControllers();
+
+app.Run();
