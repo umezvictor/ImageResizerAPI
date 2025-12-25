6836e802d0a634b56658dce8d2b29352e66b479e
Chibuzor Victor Umezuruike <41339694+umezvictor@users.noreply.github.com>
2025-12-25 07:59:32 +0200

    update

---
+++ src/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ImageResizerAPI;
+
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", message);
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,17 +1,18 @@
 using Amazon.S3;
+using ImageResizerAPI;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
 
 builder.Services.AddDefaultAWSOptions(builder.Configuration.GetAWSOptions());
 builder.Services.AddAWSService<IAmazonS3>();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -24,5 +25,5 @@ if (app.Environment.IsDevelopment())
 
 app.UseCors(policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 app.MapControllers();
-
+app.MapHub<ChatHub>("/chathub");
 app.Run();
